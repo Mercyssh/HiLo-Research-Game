feef62ec700c39e0761506822a46cbe5431fff82
Mercyssh <ima.omrane@gmail.com>
2022-11-09 05:07:52 +0530

    Established new functions / flow for Dice Roll Function
    +Gotta finish it up

--- game-logic.js
+++ game-logic.js
@@ -3,35 +3,56 @@ const lesserBtn = document.querySelector("#lesserBtn");
 const equalBtn = document.querySelector("#equalBtn");
 const greaterBtn = document.querySelector("#greaterBtn");
 
+const textContainer = document.querySelector("#textContainer");
+
+const diceContainer = document.querySelector(".diceContainer");
 const dice = document.querySelector("#dice");
+const diceNumber = document.querySelector("#diceNumber");
+
 var _rolling = false;   //used to track for tutorial end kickoff
 var rolling = false;    //used to track otherwise
-const diceRollDuration = 1500;  //Enter value in ms. Experiment to find out plz
+const diceRollDuration = 2000;  //Enter value in ms. Experiment to find out plz
 
 var currentNumber = null;
 
 // GAME LOGIC FUNCTIONS
 function rollDice() {
     if (!rolling) {
-        //Setup
-        rolling = true;
+        rolling = true; //Setup
 
-        //Switch the IMG to GIF
-        dice.src = "assets/Roll1.gif";
+        //Move Dice to Center of Screen
+        DiceToCenter(() => {
 
-        //Switch back to PNG and decide random number
-        setTimeout(() => {
-            dice.src = "assets/dice-static.png"
+            //"Play" the animation
 
 
+        });
 
-            // Cleanup
-            _rolling = false;
-            rolling = false;
-        }, diceRollDuration)
+        // //Switch the IMG to GIF
+        // dice.src = "assets/Roll1.gif";
+
+        // //Switch back to PNG and decide random number
+        // setTimeout(() => {
+        //     dice.src = "assets/dice-static.png"
+
+        //     currentNumber = getRandomInt(1, 12);
+        //     diceNumber.innerHTML = currentNumber;
+
+        //     // Cleanup
+        //     _rolling = false;
+        //     rolling = false;
+        // }, diceRollDuration)
+
+
         //Switch back to PNG
     }
 }
+function DiceToCenter(callback) {
+    textContainer.classList.add("hideTextContainer");
+    diceContainer.classList.add("diceToCenter");
+    callback();
+}
+
 
 // HELPER FUNCTIONS
 function getRandomInt(min, max) {
