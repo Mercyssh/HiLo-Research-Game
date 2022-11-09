f0d9f84170d012e5df727b2e06454c87b0f1e050
Mercyssh <ima.omrane@gmail.com>
2022-11-09 13:59:34 +0530

    working on game logic
    backup

--- game-logic.js
+++ game-logic.js
@@ -14,44 +14,75 @@ var rolling = false;    //used to track otherwise
 const diceRollDuration = 2000;  //Enter value in ms. Experiment to find out plz
 
 var currentNumber = null;
+var lastNumber = null;
+var op = null;
+
+
+//###EVENT HANDLERS
+$("#lesserBtn").click(rollDice);
+$("#equalBtn").click(rollDice);
+$("#greaterBtn").click(rollDice);
+
+//###END EVENT HANDLERS
+
+
 
 // GAME LOGIC FUNCTIONS
-function rollDice() {
+/* #region DICE ROLL SEQUENCE*/
+function rollDice(ele) {
     if (!rolling) {
         rolling = true; //Setup
 
-        //Move Dice to Center of Screen
-        DiceToCenter(() => {
+        //Cache last value
+        lastNumber = currentNumber;
 
-            //"Play" the animation
+        //Get Op from ele
+        console.log(ele);
 
+        //###Execute Sequence
+        DiceToCenter(800, () =>
+            DiceToGIF(diceRollDuration, () =>
+                DiceToPNG(500, DiceToDefault)
+            )
+        );
+        //###SequenceEnd
 
-        });
-
-        // //Switch the IMG to GIF
-        // dice.src = "assets/Roll1.gif";
-
-        // //Switch back to PNG and decide random number
-        // setTimeout(() => {
-        //     dice.src = "assets/dice-static.png"
-
-        //     currentNumber = getRandomInt(1, 12);
-        //     diceNumber.innerHTML = currentNumber;
-
-        //     // Cleanup
-        //     _rolling = false;
-        //     rolling = false;
-        // }, diceRollDuration)
-
-
-        //Switch back to PNG
     }
 }
-function DiceToCenter(callback) {
+function DiceToCenter(delay, callback) {
     textContainer.classList.add("hideTextContainer");
     diceContainer.classList.add("diceToCenter");
-    callback();
+    setTimeout(callback, delay);
 }
+function DiceToGIF(delay, callback) {
+    diceNumber.innerHTML = "";
+    dice.src = "assets/Roll1.gif";
+    setTimeout(callback, delay);
+}
+function DiceToPNG(delay, callback) {
+    //Dice just stopped Rolling. In the Center of the Screen
+    dice.src = "assets/dice-static.png"
+
+    // Get New number
+    currentNumber = getRandomInt(1, 12);
+    diceNumber.innerHTML = currentNumber;
+
+    //Compare new number with previous against user choice
+    if (lastNumber != null || op != null) {
+        var correct = eval(`${lastNumber}${op}${currentNumber}`);
+        op = null;
+        console.log(correct);
+    }
+
+    _rolling = false; rolling = false;  //cleanup
+
+    setTimeout(callback, delay);
+}
+function DiceToDefault() {
+    textContainer.classList.remove("hideTextContainer");
+    diceContainer.classList.remove("diceToCenter");
+}
+/* #endregion */
 
 
 // HELPER FUNCTIONS
