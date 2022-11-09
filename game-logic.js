955c5f33d19c993e31e630ec8f19d3ebe716192a
Mercyssh <ima.omrane@gmail.com>
2022-11-09 15:16:43 +0530

    added some region folding & Setup for score calculation

--- game-logic.js
+++ game-logic.js
@@ -1,4 +1,5 @@
 //Get Button Selectors
+/* #region   */
 const lesserBtn = document.querySelector("#lesserBtn");
 const equalBtn = document.querySelector("#equalBtn");
 const greaterBtn = document.querySelector("#greaterBtn");
@@ -9,22 +10,26 @@ const diceContainer = document.querySelector(".diceContainer");
 const dice = document.querySelector("#dice");
 const diceNumber = document.querySelector("#diceNumber");
 
+const flasher = document.querySelector("#flasher");
+/* #endregion */
+
 var _rolling = false;   //used to track for tutorial end kickoff
 var rolling = false;    //used to track otherwise
 const diceRollDuration = 2000;  //Enter value in ms. Experiment to find out plz
 
+// Trackers
 var currentNumber = null;
 var lastNumber = null;
 var op = null;
+var score = 0;
+// var maxScore = 500;
 
 
 //###EVENT HANDLERS
 $("#lesserBtn").click(rollDice);
 $("#equalBtn").click(rollDice);
 $("#greaterBtn").click(rollDice);
-
 //###END EVENT HANDLERS
-
 
 
 // GAME LOGIC FUNCTIONS
@@ -37,7 +42,8 @@ function rollDice(ele) {
         lastNumber = currentNumber;
 
         //Get Op from ele
-        console.log(ele);
+        if (ele != undefined || ele != null)
+            op = ele.target.getAttribute("data-op");
 
         //###Execute Sequence
         DiceToCenter(800, () =>
@@ -68,11 +74,7 @@ function DiceToPNG(delay, callback) {
     diceNumber.innerHTML = currentNumber;
 
     //Compare new number with previous against user choice
-    if (lastNumber != null || op != null) {
-        var correct = eval(`${lastNumber}${op}${currentNumber}`);
-        op = null;
-        console.log(correct);
-    }
+    DetermineOutcome();
 
     _rolling = false; rolling = false;  //cleanup
 
@@ -83,6 +85,32 @@ function DiceToDefault() {
     diceContainer.classList.remove("diceToCenter");
 }
 /* #endregion */
+
+//OUTCOME DETERMINATION : Tries to determine wether the guess was correct or not
+function DetermineOutcome() {
+    if (lastNumber != null || op != null || op != undefined) {
+        var correct = eval("" + lastNumber + op + currentNumber);
+        op = null;
+        console.log(correct);
+
+        // Win / Flash Animation
+        if (correct) { correctFlashSequence(); }
+        else { wrongFlashSequence(); }
+
+        // Calculate Score
+
+    }
+}
+// Call when correct guess
+function correctFlashSequence() {
+    flasher.classList.add("correctFlash");
+    setTimeout(() => { flasher.classList.remove("correctFlash"); }, 500);
+}
+// Call when wrong guess
+function wrongFlashSequence() {
+    flasher.classList.add("wrongFlash");
+    setTimeout(() => { flasher.classList.remove("wrongFlash"); }, 500);
+}
 
 
 // HELPER FUNCTIONS
